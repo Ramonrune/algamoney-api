bf4bd52500781357fd7fbd37859f87ba1e7ab18f
Ramon <ramonrune@gmail.com>
2019-03-06 21:33:08 -0300

    Adicionado suporte para autorização basic

--- src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algamoney.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,7 +18,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Profile("oauth-security")
 @Configuration
 @EnableWebSecurity
 @EnableResourceServer
