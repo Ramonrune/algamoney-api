0373aa31d41128ab572133b5f469e5d239205edd
Ramon <ramonrune@gmail.com>
2019-05-01 19:05:31 -0300

    origin

--- src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
-	private String originPermitida = "http://localhost:8000";
+	private String originPermitida = "http://localhost:4200";
 	
 	private Seguranca seguranca = new Seguranca();
 
